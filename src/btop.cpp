d9a3edfdaf74b61c2fa78cda724ec59a8e62152a
bot984397 <vmx@0x6e63.com>
2025-05-20 14:07:15 +0200

    bugfix - clean_quit() [btop.cpp:195]

--- src/btop.cpp
+++ src/btop.cpp
@@ -193,8 +193,11 @@ void term_resize(bool force) {
 
 //* Exit handler; stops threads, restores terminal and saves config changes
 void clean_quit(int sig) {
-	if (Global::quitting) return;
-	Global::quitting = true;
+   //? fixed race condition due to improper atomic handling
+   bool expected = false;
+   if (!Global::quitting.compare_exchange_strong(expected, true)) {
+      return;
+   }
 	Runner::stop();
 	if (Global::_runner_started) {
 	#if defined __APPLE__ || defined __OpenBSD__ || defined __NetBSD__
